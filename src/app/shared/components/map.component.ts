bbc664311da23ec44902f4c6a71fdfc693ad3d6f
Gian20luca <gianluca.bellafronte@gmail.com>
2020-12-23 18:02:00 +0100

    first commit

--- src/app/shared/components/map.component.ts
+++ src/app/shared/components/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import * as L from 'leaflet';
 import { HttpClient } from '@angular/common/http';
 import { GeoJsonObject } from 'geojson';
@@ -11,21 +11,22 @@ import { forkJoin } from 'rxjs';
       leaflet
       [leafletOptions]="options"
       (leafletMapReady)="onMapReady($event)"
-      style="height: 500px; width: 400px;"
     >
-      <div id="map"></div>
+      <div id="map" style="height: 500px; width: 400px;"></div>
     </div>
+    <button *ngIf="this.risposta" (click)="ShowAllPositive(this.risposta)">Mostra tutti i positivi</button>
   `,
   styles: [],
 })
-export class MapComponent implements OnInit, AfterViewInit {
+export class MapComponent implements OnInit {
   map: L.Map;
 
   bounds1 = L.latLng(42, 13);
   bounds2 = L.latLng(42, 12);
   options;
-  public resGeojson: any;
-  public resJson: any;
+  risposta;
+  public resGeojson: GeoJsonObject;
+  public resJson: GeoJsonObject;
 
   constructor(private http: HttpClient) {}
 
@@ -42,46 +43,84 @@ export class MapComponent implements OnInit, AfterViewInit {
       center: L.latLng(42, 13),
       maxBounds: L.latLngBounds(this.bounds1, this.bounds2),
     };
-
-    this.callServer().subscribe((res) => {
-      this.resGeojson = res[0];
-      this.resJson = res[1];
-      console.log(res);
-
-      if (
-        this.resGeojson.features[0].properties.reg_name === this.resJson[0].name &&
-        this.resJson[0].positive > 100
-      ) {
-        console.log('Zona rossa');
-      }
-      else {console.log("Zona gialla")}
-
-    });
   }
 
   onMapReady(map: L.Map) {
-    //chiamata all' url del geojson
-    this.http
-      .get(
-        'https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_regions.geojson'
-      )
-      .subscribe((data: GeoJsonObject) => L.geoJSON().addTo(map));
 
-    console.log;
-  }
 
-  ngAfterViewInit(): void {
-    //console log della risposta
-  }
 
-  callServer(): any {
-    //chiamata geojson
-    let geojson = this.http.get(
+
+    let MyJson = this.http.get<GeoJsonObject>('http://localhost:3000/regione');
+    let GeoJson = this.http.get<GeoJsonObject>(
       'https://raw.githubusercontent.com/openpolis/geojson-italy/master/geojson/limits_IT_regions.geojson'
     );
-    //chiamata json
-    let json = this.http.get('http://localhost:3000/regione');
+    forkJoin([MyJson, GeoJson]).subscribe((res) => {
+      // res[0] is our MyJson
+      // res[1] is our GeoJson
+      console.log(res);
+      this.risposta = res;
+      function controlNumber(positive) {
+        if (positive <= 500) {
+          return {
+            color: 'black',
+            fillColor: 'yellow',
+            fillOpacity: 1,
+            opacity: 1,
+            weight: 0.75,
+          };
+        }
+        if (positive > 500 && positive <= 10000) {
+          return {
+            color: 'black',
+            fillColor: 'orange',
+            fillOpacity: 1,
+            opacity: 1,
+            weight: 0.75,
+          };
+        }
+        if (positive > 10000) {
+          return {
+            color: 'black',
+            fillColor: 'red',
+            fillOpacity: 1,
+            opacity: 1,
+            weight: 0.75,
+          };
+        }
+      }
 
-    return forkJoin([geojson, json]);
+
+      L.geoJSON(res[1], {
+        onEachFeature: (features, layer) => {
+          layer.bindTooltip(features.properties.reg_name);
+        },
+
+        style: (features) => {
+          for (var i = 0; i < 20; i++) {
+            switch ((features.properties.reg_istat_code_num - 1) as number) {
+              case i:
+                return controlNumber(
+                  res[0][features.properties.reg_istat_code_num - 1].positive
+                );
+            }
+          }
+        },
+      }).addTo(map);
+    });
   }
+
+ShowAllPositive(res){
+  console.log("a")
+  L.geoJSON(res[1],{
+  onEachFeature: function (features, layer){
+    layer.bindTooltip("<span style='font-size:100px'>"+features.properties.reg_name+ "<br>"+ res[0][features.properties.reg_istat_code_num-1].positive+"</span>", {
+      permanent: true,
+      direction: "center",
+      className: "labelRegioni"
+
+    }).openTooltip();
+  }
+}).addTo(this.map);
+
+}
 }
